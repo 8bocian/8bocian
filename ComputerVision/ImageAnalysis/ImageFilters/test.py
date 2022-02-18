41a5951ab20118541279b2b330213e022e38d95e
Stork <74800537+8bocian@users.noreply.github.com>
2022-02-18 10:08:07 +0100

    Create test.py

---
+++ ComputerVision/ImageAnalysis/ImageFilters/test.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2022. Oskar "Bocian" Możdżeń
+#  All rights reserved.
+
+from cv2 import cv2
+from SobelFilter import sobelFiltering
+from GaussianFilter import gaussianFilter
+
+image = cv2.imread("../Images/EmpireState/image4.jpg")
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+sobel_image = sobelFiltering(image_gray)
+
+gaussian_image = gaussianFilter(image_gray, sigma=3)
+
+cv2.imshow('Sobel image', sobel_image)
+cv2.waitKey(0)
+cv2.imshow('Gaussian image', gaussian_image)
+cv2.waitKey(0)
